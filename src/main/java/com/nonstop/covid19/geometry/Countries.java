31611d9e60d7cd8a92e5546869786f231ea03e99
imkiva <libkernelpanic@gmail.com>
2020-03-25 00:30:38 +0800

    geometry: return list instead of map

--- src/main/java/com/nonstop/covid19/geometry/Countries.java
+++ src/main/java/com/nonstop/covid19/geometry/Countries.java
@@ -3,19 +3,16 @@ package com.nonstop.covid19.geometry;
 import com.nonstop.covid19.api.Day;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Countries {
-    public static Map<String, Country> from(List<Day> countries) {
+    public static List<Country> from(List<Day> countries) {
         return countries.stream()
                 .collect(Collectors.groupingBy(Day::getCountry))
                 .entrySet()
                 .stream()
                 .filter(e -> CountryGeo.contains(e.getKey()))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        e -> new Country(e.getKey(), e.getValue())
-                ));
+                .map(e -> new Country(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/nonstop/covid19/TestGeometry.java
+++ src/test/java/com/nonstop/covid19/TestGeometry.java
@@ -5,15 +5,12 @@ import com.nonstop.covid19.api.VirusServices;
 import com.nonstop.covid19.geometry.Countries;
 import org.junit.Test;
 
-import java.util.Map;
-
 public class TestGeometry {
     @Test
     public void testGeoData() {
         VirusService service = VirusServices.create();
         try {
             Countries.from(service.allCountries().execute().body())
-                    .values()
                     .forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();

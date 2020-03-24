3f1114946ef365f4def2c7eb89958b5f26784b2a
imkiva <libkernelpanic@gmail.com>
2020-03-24 18:38:01 +0800

    cli: add cli support

---
+++ src/main/java/com/nonstop/covid19/cli/Main.java
@@ -0,0 +1,8 @@
+package com.nonstop.covid19.cli;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Corona Virus 2019 Visualizer CLI");
+        System.out.println("Nice to see you :)");
+    }
+}
